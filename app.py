b782a5d04484bb5135b8d708f06d08be3d40c531
juliebcrowe <77651204+juliebcrowe@users.noreply.github.com>
2023-04-30 18:52:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ facet_grid.show()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     [
